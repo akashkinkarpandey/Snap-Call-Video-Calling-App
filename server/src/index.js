b50599e94bdcb66e869751ba9f165d1f953b820a
Akash Kinkar Pandey <akashkinkarpandey@gmail.com>
2025-08-31 16:26:36 +0530

    Build fixes for server

---
+++ server/src/index.js
@@ -0,0 +1,72 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const http_1 = require("http");
+const socket_io_1 = require("socket.io");
+const app = (0, express_1.default)();
+const server = (0, http_1.createServer)(app);
+const io = new socket_io_1.Server(server, {
+    cors: {
+        origin: "*",
+    },
+});
+const port = parseInt(process.env.PORT || "8080", 10);
+app.use(express_1.default.json());
+app.get("/health", (req, res) => {
+    res.status(200).json({
+        status: "OK",
+        timestamp: new Date().toISOString(),
+        uptime: process.uptime(),
+    });
+});
+const userToRoomMap = new Map();
+io.on("connection", (socket) => {
+    console.log("Socket connected - " + socket.id);
+    socket.on("room-join", (roomID) => {
+        const room = io.sockets.adapter.rooms.get(roomID);
+        if (room === undefined) {
+            socket.join(roomID);
+            socket.emit("room-created");
+        }
+        else if (room.size === 1) {
+            socket.join(roomID);
+            socket.emit("room-joined");
+        }
+        else {
+            socket.emit("room-full");
+            return;
+        }
+        if (!userToRoomMap.has(socket.id)) {
+            userToRoomMap.set(socket.id, new Set([roomID]));
+        }
+        else {
+            userToRoomMap.get(socket.id)?.add(roomID);
+        }
+    });
+    socket.on("ready", (roomID) => {
+        socket.to(roomID).emit("ready");
+    });
+    socket.on("message", (message, roomID) => {
+        socket.to(roomID).emit("message", message);
+    });
+    socket.on("id2Content", (id2Content, roomID) => {
+        console.log("id2Content", id2Content);
+        socket.to(roomID).emit("id2Content", id2Content);
+    });
+    socket.on("disconnect", () => {
+        console.log("Socket disconnected - " + socket.id);
+        const rooms = userToRoomMap.get(socket.id);
+        if (rooms) {
+            rooms.forEach((roomID) => {
+                socket.to(roomID).emit("user-disconnected", socket.id);
+            });
+        }
+        userToRoomMap.delete(socket.id);
+    });
+});
+server.listen(port, () => {
+    console.log("Server listening at PORT " + port);
+});
